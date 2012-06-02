323e1a355b1516468d5e635ae32f0aa79a00e58c
byplayer <byplayer100@gmail.com>
2012-06-03 02:06:44 +0900

    remove white space

--- egg.el
+++ egg.el
@@ -1,18 +1,18 @@
 ;;; egg.el -- Emacs Got Git
 ;;
 ;; A magit fork
-;; 
+;;
 ;; Copyright (C) 2008  Linh Dang
 ;; Copyright (C) 2008  Marius Vollmer
 ;; Copyright (C) 2009  Tim Moore
 ;; Copyright (C) 2010  Alexander Prusov
 ;; Copyright (C) 2011  byplayer
-;; 
+;;
 ;; Author: Bogolisk <bogolisk@gmail.com>
 ;; Created: 19 Aug 2008
 ;; Version: 1.0.2
 ;; Keywords: git, version control, release management
-;; 
+;;
 ;; Special Thanks to
 ;;   Antoine Levitt, Bogolisk,
 ;;   Christian Köstlin
