2c862e7be53d8af2ee7bd7f5d58620f9c9d8d955
byplayer <byplayer100@gmail.com>
2012-09-28 11:11:21 +0900

    fix merge problem

--- egg.el
+++ egg.el
@@ -479,23 +479,6 @@ desirable way to invoke GIT."
   :group 'egg
   :type 'string)
 
-(defcustom egg-patch-command "patch"
-  "Name or full-path to the patch command.
-Set this to the appropriate string in the case where `patch' is not the
-desirable way to invoke gnu patch command."
-  :group 'egg
-  :type 'string)
-
-
-(defcustom egg-patch-command "patch"
-  "Name or full-path to the patch command.
-Set this to the appropriate string in the case where `patch' is not the
-desirable way to invoke gnu patch command."
-  :group 'egg
-  :type 'string)
-
-
-
 (defcustom egg-dummy-option nil
   "Foo bar"
   :group 'egg
@@ -858,22 +841,8 @@ END-RE is the regexp to match the end of a record."
   "get the SHA1 of REV."
   (egg-git-to-string "rev-parse" (concat rev "~0")))
 
-(defun egg-win32-clean-cygwin-path (dir)
-  ;; from csebold
-  ;; replace cygwin data if being returned to a non-cygwin Emacs
-  (save-match-data
-    (if (string-match ":\\(/cygdrive/[a-z]+\\)" dir)
-	(replace-match "" t t dir 1)
-      dir)))
-
 (defun egg-read-git-dir ()
   "call GIT to read the git directory of default-directory."
-  (let ((dir (egg-git-to-string "rev-parse" "--git-dir")))
-    (if (stringp dir) 
-	(expand-file-name dir))))
-  (let ((dir (egg-git-to-string "rev-parse" "--git-dir")))
-    (if (stringp dir) 
-	(funcall egg-compute-git-dir-func (expand-file-name dir)))))
   (let* ((dotgit-parent (locate-dominating-file default-directory ".git"))
 	 (dotgit (and dotgit-parent (concat dotgit-parent "/.git")))
          (dir (or (and dotgit (file-directory-p dotgit) dotgit)
