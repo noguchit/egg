b4b5b575bb0c66cacc38859adb3035b5918f4355
Bogolisk <bogolisk@gmail.com>
2012-11-04 08:21:28 -0500

    patch computing now working properly

--- egg-diff.el
+++ egg-diff.el
@@ -24,39 +24,39 @@
 
 (require 'egg)
 
-(defun egg--inline-diff-del-block-to-patch (pos &optional old-rev)
-  (let ((old-rev (or old-rev ":0"))
-	(git-name (egg-buf-git-name))
+(defun egg--inline-diff-mk-patch (file rev line del-num add-num add-text)
+  (let ((git-name (egg-file-git-name file))
 	(dir (egg-work-tree-dir))
-	(old-line (get-text-property pos :old-line))
-	(num (get-text-property pos :num))
 	(pre-lines 0)
 	(post-lines 0)
+	(del-num (or del-num 0))
+	(add-num (or add-num 0))
 	pre post beg end text)
 
     (with-temp-buffer
-      (egg-git-ok t "--no-pager" "show" (concat old-rev ":" git-name))
-      (goto-char (egg-line-2-pos old-line))
+      (setq default-directory dir)
+      (egg-git-ok t "--no-pager" "show" (concat rev ":" git-name))
+      (goto-char (egg-line-2-pos line))
       (while (and (not (bobp)) (< pre-lines 3))
 	(forward-line -1)
 	(setq pre-lines (1+ pre-lines)))
       (setq beg (point))
-      (goto-char (egg-line-2-pos old-line num))
+      (goto-char (egg-line-2-pos line del-num))
+      (when add-text (insert add-text))
       (while (and (not (eobp)) (< post-lines 3))
 	(forward-line 1)
 	(setq post-lines (1+ post-lines)))
       (setq end (point))
       (setq text (buffer-substring-no-properties beg end)))
 
-    (with-current-buffer "*foo*"
-      (erase-buffer)
+    (with-temp-buffer
       (insert (format "diff a/%s b/%s\n" git-name git-name)
 	      (format "--- a/%s\n+++ b/%s\n" git-name git-name)
 	      (format "@@ -%d,%d +%d,%d @@\n"
-		      (- old-line pre-lines)
-		      (+ pre-lines num post-lines)
-		      (- old-line pre-lines)
-		      (+ pre-lines post-lines)))
+		      (- line pre-lines)
+		      (+ pre-lines del-num post-lines)
+		      (- line pre-lines)
+		      (+ pre-lines add-num post-lines)))
       (setq beg (point))
       (insert text)
       (setq end (point))
@@ -64,23 +64,59 @@
       (dotimes (i pre-lines)
 	(insert " ")
 	(forward-line 1))
-      (dotimes (i num)
+      (dotimes (i del-num)
 	(insert "-")
+	(forward-line 1))
+      (dotimes (i add-num)
+	(insert "+")
 	(forward-line 1))
       (dotimes (i post-lines)
 	(insert " ")
-	(forward-line 1)))))
+	(forward-line 1))
+      (buffer-string))))
+
+(defun egg--inline-diff-block2patch (rev del-pos add-pos)
+  (let* ((file (buffer-file-name))
+	(del-nav (and del-pos (get-text-property del-pos :navigation)))
+	(add-nav (and add-pos (get-text-property add-pos :navigation)))
+	(del-line (and del-pos (get-text-property del-pos :old-line)))
+	(add-line (and add-pos (get-text-property add-pos :orig-line)))
+	(del-num (and del-pos (get-text-property del-pos :num)))
+	(add-num (and add-pos (get-text-property add-pos :num)))
+	add-text add-text-info patch)
+
+    (unless (or del-line add-line)
+      (error "No info found to build patch: positions %d/%d" del-pos add-pos))
+
+    (unless (or (null del-line) (null add-line)
+		(= (+ del-line del-num) add-line))
+      (error "Cannot merge separate diff blocks: -%d and +%d" del-line add-line))
+    
+    (when add-pos
+      (setq add-text-info (assq add-nav (plist-get egg-inline-diff-info :new-text)))
+      (setq add-text (buffer-substring-no-properties (nth 1 add-text-info)
+						     (nth 2 add-text-info))))
+    (setq patch
+	  (egg--inline-diff-mk-patch file rev (or del-line add-line)
+				     del-num add-num add-text))
+    (with-current-buffer "*foo*"
+      (erase-buffer)
+      (insert patch))
+    patch))
+
 
 (defvar egg-inline-diff-info nil)
 
-(defun egg-compute-file-inline-diff (file-name)
+(defun egg-inline-diff-compute-info (file-name)
   (let (hunk beg ranges all text-beg block-lines prev-line-no
 	     block-start line-no old-start old-line-no
 	     current-prefix start-c no-diff) 
     (with-temp-buffer
       (setq no-diff
 	    (egg-git-ok t "--no-pager" "diff" "--exit-code" 
-			"--function-context" "--unified=5" "--" file-name))
+			;;"--function-context"
+			;;"--unified=5"
+			"--" file-name))
       (unless no-diff
 	(goto-char (point-max))
 	(save-match-data
@@ -90,11 +126,14 @@
 			       (split-string (match-string-no-properties 0) "[-+,@ ]+" t)))
 	    (setq line-no (1- (nth 2 hunk)))
 	    (setq old-line-no (1- (nth 0 hunk)))
+	    (setq old-start nil)
 	    (while (not (eobp))
 	      (forward-line 1)
 	      (setq start-c (char-after (point)))
 	      (unless (eobp) (delete-char 1))
 
+	      ;; keep the prev-line-no because
+	      ;; line-no is conditionally incremented
 	      (setq prev-line-no line-no)
 	      (when (memq start-c '(?- ? )) (setq old-line-no (1+ old-line-no)))
 	      (when (memq start-c '(?+ ? )) (setq line-no (1+ line-no)))
@@ -102,20 +141,36 @@
 	      (if (eq current-prefix start-c)
 		  (setq block-lines (1+ block-lines))
 
-		;; keep removed text because it's not in the file
-		(when (eq start-c ?-) (setq text-beg (point)))
-
+		;; switching
+		
+		;; building info
 		(cond ((eq current-prefix ? ) nil)
 		      ((eq current-prefix ?+)
-		       (push (list :add block-start block-lines) ranges))
+		       (push (list :add block-start block-lines old-start) ranges))
 		      ((eq current-prefix ?-)
 		       (push (list :del block-start block-lines old-start
 				   (buffer-substring-no-properties text-beg (point)))
 			     ranges)))
+
+		(cond ((eq start-c ? )
+		       ;; restart position in the original file
+		       (setq old-start nil))
+		      ((eq start-c ?-)
+		       ;; keep removed text because it's not in the file
+		       (setq text-beg (point))
+		       ;; del-only or combined hunk, this is the starting point
+		       ;; of the changes in the original file
+		       (setq old-start old-line-no))
+		      ((eq start-c ?+)
+		       ;; the strting point in the original file is either:
+		       ;;  - the beginning of the add-only lines
+		       ;;  - or the end of the del-lines
+		       ;; +1 because old-line-no doesn't advance this time
+		       (setq old-start (1+ old-line-no))))
+
 		;; start new block
 		(setq block-lines 1)
 		(setq current-prefix start-c)
-		(setq old-start old-line-no)
 		;; in the case of del block, line-no would have stayed the same
 		;; but we must move the block start ahead
 		(setq block-start (1+ prev-line-no))))
@@ -145,10 +200,10 @@
 (defun egg-do-buffer-inline-diff ()
   (let ((read-only-state buffer-read-only)
 	(inhibit-read-only t)
-	(ranges (egg-compute-file-inline-diff (buffer-file-name)))
+	(ranges (egg-inline-diff-compute-info (buffer-file-name)))
 	beg end
 	type line num text old-line
-	old-text-list ov-list ov)
+	old-text-list new-text-list ov-list ov)
     (if (null ranges)
 	(message "no differences in %s" (buffer-file-name))
       (widen)
@@ -169,19 +224,22 @@
 	       (add-text-properties beg end
 				    (list :navigation line
 					  'invisible line
-					  :new-line line
 					  'keymap egg-section-map))
 	       (egg--inline-diff-mk-boundaries-visible beg end))
 	      ((eq type :add)
 	       (add-text-properties beg end
 				    (list :navigation line
 					  'invisible line
+					  :orig-line old-line
 					  :num num))
 	       (egg--inline-diff-mk-boundaries-visible beg end)
 	       (setq ov (make-overlay beg end nil t nil))
 	       (overlay-put ov 'face 'egg-add-bg)
 	       (overlay-put ov 'evaporate t)
-	       (push ov ov-list))
+	       (push ov ov-list)
+	       (setq beg (copy-marker beg t))
+	       (setq end (copy-marker end t))
+	       (push (list line beg end) new-text-list))
 	      ((eq type :del)
 	       (goto-char beg)
 	       (insert text)
@@ -197,12 +255,14 @@
 	       (overlay-put ov 'evaporate t)
 	       (push ov ov-list)
 	       (setq beg (copy-marker beg t))
-	       (setq end (point-marker))
+	       (setq end (copy-marker end t))
 	       (push (list (- line) beg end) old-text-list))))
       (setq buffer-invisibility-spec (list '(0 . t)))
       (set (make-local-variable 'egg-inline-diff-info)
 	   (list :read-only read-only-state
-		 :overlays ov-list :old-text old-text-list))
+		 :overlays ov-list 
+		 :old-text old-text-list
+		 :new-text new-text-list))
       (set-buffer-modified-p nil)
       (setq buffer-read-only t))))
 
